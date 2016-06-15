65ad3264424e9d3f33e2af934fbadbf4bfe9ab56
Joseph C Wang <joequant@gmail.com>
2016-06-15 17:56:57 +0800

    add more scripts

--- install-ipfs.sh
+++ install-ipfs.sh
@@ -2,5 +2,8 @@
 
 export GOPATH=~/golang
 go get -d github.com/ipfs/go-ipfs
-cd $GOPATH/src/github.com/ipfs/go-ipfs
+pushd $GOPATH/src/github.com/ipfs/go-ipfs
 make install
+popd
+
+npm install --save ipfs-api
---
+++ test.js
@@ -0,0 +1,3 @@
+#!/usr/bin/node
+var ipfsAPI = require('ipfs-api')
+var ipfs = ipfsAPI('localhost', '5001', {protocol: 'http'})
